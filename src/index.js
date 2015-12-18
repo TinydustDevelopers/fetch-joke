99efe08e05fa6f68d4e4fc372bb5f4e5de27e2ed
PortWatcher <root@pwhack.me>
2015-12-18 16:43:22 +0800

    initial commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+var request = require('request')
+var restify = require('restify')
+
+const API_URL = 'http://api.laifudao.com/open/xiaohua.json'
+
+var app = restify.createServer({
+  name: 'jokes'
+})
+
+app.get('/joke', (req, res) => {
+  request(API_URL, (err, response, body) => {
+    if (err) {
+      console.error(err)
+      return res.send({type: 'Error', data: [{
+        code: err.code,
+        message: err.toString()
+      }]})
+    }
+
+    if (response.statusCode === 200) {
+      var jokes = JSON.parse(body)
+      var joke = jokes[Math.floor(Math.random() * jokes.length)]
+
+      return res.send({type: 'Text', data: [ joke.content.trim().replace(/"/g, '\"').replace(/<br\/>/g, '') ]})
+    } else {
+      return res.send({type: 'Error', data: [{
+        code: response.statusCode,
+        message: response.statusCode
+      }]})
+    }
+  })
+})
+
+app.listen(3000, () => {
+  console.log('listening at 3000')
+})
