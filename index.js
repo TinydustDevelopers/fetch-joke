887ad79c05ee2e130abb416605b2ee7172e572f5
PortWatcher <root@pwhack.me>
2015-12-19 11:48:41 +0800

    update error handler

--- index.js
+++ index.js
@@ -11,22 +11,31 @@ app.get('/joke', (req, res) => {
   request(API_URL, (err, response, body) => {
     if (err) {
       console.error(err);
-      return res.send({ type: 'Error', data: [{
-          code: err.code,
+      return res.send({
+        type: 'Error',
+        data: [{
+          code: 102,
           message: err.toString()
-        }] });
+        }]
+      });
     }
 
     if (response.statusCode === 200) {
       var jokes = JSON.parse(body);
       var joke = jokes[Math.floor(Math.random() * jokes.length)];
 
-      return res.send({ type: 'Text', data: [joke.content.trim().replace(/"/g, '\"').replace(/<br\/>/g, '')] });
+      return res.send({
+        type: 'Text',
+        data: [joke.content.trim().replace(/"/g, '\"').replace(/<br\/>/g, '')]
+      });
     } else {
-      return res.send({ type: 'Error', data: [{
-          code: response.statusCode,
-          message: response.statusCode
-        }] });
+      return res.send({
+        type: 'Error',
+        data: [{
+          code: 601,
+          message: body
+        }]
+      });
     }
   });
 });
